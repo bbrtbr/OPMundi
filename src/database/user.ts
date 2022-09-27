4c1c5d26ef4192ca4164e56888eda6ef94dc98c0
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-09-27 20:49:45 -0300

    feat(database): Created user database actions

---
+++ src/database/user.ts
@@ -0,0 +1,13 @@
+import { database } from '../utils/firebase'
+import type { User } from '../models/user'
+import { ref, set, get } from 'firebase/database'
+
+export const insertUserIntoDatabase = async (user: User) => {
+  const databaseReference = ref(database, `users/${user.uid}`)
+  await set(databaseReference, user)
+}
+
+export const getUserFromDatabase = async (uid: string): Promise<User> => {
+  const databaseReference = ref(database, `users/${uid}`)
+  return (await (await get(databaseReference)).val()) as User
+}
