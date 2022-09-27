d392ca7df08f1a6d03c5b1a58a3305c72b84b88a
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-09-27 19:46:45 -0300

    style: Changed file extension

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import { NativeBaseProvider, StatusBar } from 'native-base'
 import {
   useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold
+  Roboto_400Regular as roboto400Regular,
+  Roboto_700Bold as roboto700Bold
 } from '@expo-google-fonts/roboto'
 import { UserContextProvider } from './src/contexts/UserContext'
 import { THEME } from './src/components/theme'
 import { Loading } from './src/components/loading'
 import Routes from './src/routes'
+
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+  const [fontsLoaded] = useFonts({
+    robotoRegular: roboto400Regular,
+    robotoBold: roboto700Bold
+  })
 
   return (
     <NativeBaseProvider theme={THEME}>
