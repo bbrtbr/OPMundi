85ba2d5584da279b950e63604d0e00763b1226c6
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-09-27 20:50:36 -0300

    refactor(contexts): Implemented user database actions methods

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,8 +1,9 @@
 import React, { FC, createContext, ReactNode, useState, useEffect } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { ToastAndroid } from 'react-native'
-import { ref, set } from 'firebase/database'
-import { auth, database } from '../utils/firebase'
+import type { User } from '../models/user'
+import { getUserFromDatabase, insertUserIntoDatabase } from '../database/user'
+import { auth } from '../utils/firebase'
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
@@ -21,14 +22,16 @@ export const UserContextProvider: FC<UserContextProviderProps> = ({
   const [user, setUser] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
 
-  const signIn = async (email, password) => {
+  const signIn = async (email: string, password: string) => {
     setIsLoading(true)
     await signInWithEmailAndPassword(auth, email, password)
-      .then(userCredential => {
-        const user = userCredential.user
+      .then(async userCredential => {
+        const userFromAuthCredential = userCredential.user
+        const { uid } = userFromAuthCredential
+        const user: User = await getUserFromDatabase(uid)
         ToastAndroid.show('Login realizado com sucesso', ToastAndroid.SHORT)
         AsyncStorage.setItem('@user', JSON.stringify(user))
-        setUser(user)
+        setUser(userFromAuthCredential)
       })
       .catch(error => {
         if (error.code === 'auth/user-not-found') {
@@ -48,33 +51,38 @@ export const UserContextProvider: FC<UserContextProviderProps> = ({
   }
 
   const registerAccount = async (
-    email,
-    password,
-    Name,
-    emailA,
-    Telefone,
-    Gender,
-    uf,
-    cityA,
-    districtA
+    email: string,
+    password: string,
+    name: string,
+    phone: string,
+    sex: string,
+    state: string,
+    city: string,
+    district: string
   ) => {
     setIsLoading(true)
     await createUserWithEmailAndPassword(auth, email, password)
       .then(async userCredential => {
-        const user = userCredential.user
-        setIsLoading(false)
-        await AsyncStorage.setItem('@user', JSON.stringify(user))
-        setUser(user)
-        ToastAndroid.show('Registrado com sucesso', ToastAndroid.SHORT)
-        const databaseReference = ref(database, 'users/' + user.uid)
-        set(databaseReference, {
-          name: Name,
-          email: emailA,
-          phone: Telefone,
-          sex: Gender,
-          state: uf,
-          city: cityA,
-          district: districtA
+        const userFromAuthCredential = userCredential.user
+        const { uid } = userFromAuthCredential
+        const user: User = {
+          uid,
+          email,
+          name,
+          phone,
+          sex,
+          state,
+          city,
+          district
+        }
+        await insertUserIntoDatabase(user).then(async () => {
+          setIsLoading(false)
+          await AsyncStorage.setItem(
+            '@user',
+            JSON.stringify(userFromAuthCredential)
+          )
+          setUser(userFromAuthCredential)
+          ToastAndroid.show('Registrado com sucesso', ToastAndroid.SHORT)
         })
       })
       .catch(error => {
