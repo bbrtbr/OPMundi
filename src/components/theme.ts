dd113b2f65a2cb2f0ba547e47777457a4588984b
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-09-27 19:47:17 -0300

    refactor: Changed font names

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -25,8 +25,8 @@ export const THEME = extendTheme({
     white: '#FFFFFF'
   },
   fonts: {
-    heading: 'Roboto_700Bold',
-    body: 'Roboto_400Regular'
+    heading: 'roboto700Bold',
+    body: 'roboto400Regular'
   },
   fontSizes: {
     xs: 12,
