48c1eca704d20e15b9d350799d8e61c54849d2ae
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-09-29 02:49:42 -0300

    refactor(pages): Refactored Profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,14 +1,36 @@
-import React, { useState, useContext, useEffect } from 'react'
-
-import { VStack, Heading, Icon, useTheme, Image } from 'native-base'
+import React, { useContext } from 'react'
+import { VStack, Heading, Text, Divider } from 'native-base'
 import { UserContext } from '../contexts/UserContext'
+import type { User } from '../models/user'
 import { Button } from '../components/button'
 export function Profile() {
+  const { user }: { user: User } = useContext(UserContext)
   const { signOut } = useContext(UserContext)
 
   return (
-    <VStack flex={1} alignItems="center" bg="white" px={8} pt={24}>
-      <Button width={'100%'} title="Sair" onPress={signOut}>
+    <VStack flex={1} p={4}>
+      <Heading>Meus Dados</Heading>
+      <Divider my={4} />
+      <Text fontSize={'lg'}>Nome: {user.name}</Text>
+      <Text fontSize={'lg'} my={1}>
+        E-mail: {user.email}
+      </Text>
+      <Text fontSize={'lg'} my={1}>
+        Telefone: {user.phone}
+      </Text>
+      <Text fontSize={'lg'} my={1}>
+        Sexo: {user.sex}
+      </Text>
+      <Text fontSize={'lg'} my={1}>
+        Estado: {user.state}
+      </Text>
+      <Text fontSize={'lg'} my={1}>
+        Cidade: {user.city}
+      </Text>
+      <Text fontSize={'lg'} my={1}>
+        Bairro: {user.district}
+      </Text>
+      <Button width={'100%'} title="Sair" mt={10} onPress={signOut}>
         Sair
       </Button>
     </VStack>
